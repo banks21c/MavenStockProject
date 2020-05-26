b97b62abd3a0fbb4fa689267a8dc68b4f0fa00bf
banksfamily <banksfamily@192.168.1.86>
2020-05-26 11:29:37 +0900

    m.sedaily.com 신규

---
+++ src/main/java/html/parsing/stock/news/MSedailyCom.java
@@ -0,0 +1,294 @@
+package html.parsing.stock.news;
+
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.swing.JOptionPane;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import html.parsing.stock.JsoupChangeAhrefElementsAttribute;
+import html.parsing.stock.JsoupChangeImageElementsAttribute;
+import html.parsing.stock.JsoupChangeLinkHrefElementsAttribute;
+import html.parsing.stock.JsoupChangeScriptSrcElementsAttribute;
+import html.parsing.stock.StockUtil;
+import html.parsing.stock.util.FileUtil;
+
+public class MSedailyCom extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private static Logger logger = null;
+    final static String userHome = System.getProperty("user.home");
+
+    String strYear = new SimpleDateFormat("yyyy", Locale.KOREAN).format(new Date());
+    int iYear = Integer.parseInt(strYear);
+
+    // String strYMD = new SimpleDateFormat("yyyy년 M월 d일 E ",
+    // Locale.KOREAN).format(new Date());
+    static String strYMD = "";
+    static String strDate = null;
+    static String strTitle = null;
+
+    DecimalFormat df = new DecimalFormat("###.##");
+
+    private javax.swing.JButton executeBtn;
+    private javax.swing.JButton eraseBtn;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel urlLbl;
+    private javax.swing.JTextField urlTf;
+    private javax.swing.JPanel executeResultPnl;
+    private static javax.swing.JLabel executeResultLbl;
+
+    MSedailyCom(int i) {
+        logger = LoggerFactory.getLogger(this.getClass());
+
+
+        String url = JOptionPane.showInputDialog(this.getClass().getSimpleName()+" URL을 입력하여 주세요.");
+        logger.debug("url:[" + url + "]");
+        if (StringUtils.defaultString(url).equals("")) {
+            url = "https://m.sedaily.com/NewsView/1Z2UMQZA1Y";
+        }
+        createHTMLFile(url);
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // new NewsMoneyToday(1);
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(NewsReader.class.getName()).log(java.util.logging.Level.SEVERE, null,
+                    ex);
+        }
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+			public void run() {
+                new MSedailyCom().setVisible(true);
+            }
+        });
+    }
+
+    public MSedailyCom() {
+        logger = LoggerFactory.getLogger(this.getClass());
+
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        jPanel2 = new javax.swing.JPanel();
+        urlLbl = new javax.swing.JLabel();
+        urlTf = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel(new java.awt.FlowLayout());
+        executeBtn = new javax.swing.JButton();
+        eraseBtn = new javax.swing.JButton();
+        executeResultPnl = new javax.swing.JPanel();
+        executeResultPnl.setSize(100, 50);
+        executeResultLbl = new javax.swing.JLabel("추출준비");
+        executeResultLbl.setSize(100, 50);
+        executeResultPnl.add(executeResultLbl);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("웹페이지 추출");
+        setAlwaysOnTop(true);
+        setMinimumSize(new java.awt.Dimension(500, 54));
+        setSize(new java.awt.Dimension(500, 100));
+
+        jPanel2.setLayout(new java.awt.BorderLayout());
+
+        urlLbl.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        urlLbl.setText("주소 : ");
+        urlLbl.setToolTipText("");
+        jPanel2.add(urlLbl, java.awt.BorderLayout.WEST);
+
+        urlTf.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        urlTf.setToolTipText("");
+        urlTf.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+                urlTfActionPerformed(evt);
+            }
+        });
+        urlTf.addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+			public void keyReleased(java.awt.event.KeyEvent evt) {
+                urlTfKeyReleased(evt);
+            }
+        });
+        jPanel2.add(urlTf, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(jPanel2, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(executeResultPnl, java.awt.BorderLayout.EAST);
+
+        eraseBtn.setText("지우기");
+        eraseBtn.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+                eraseBtnActionPerformed(evt);
+            }
+        });
+        jPanel1.add(eraseBtn);
+
+        executeBtn.setText("페이지 추출");
+        executeBtn.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+                executeBtnActionPerformed(evt);
+            }
+        });
+        jPanel1.add(executeBtn);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.PAGE_END);
+
+        pack();
+    }
+
+    private void executeBtnActionPerformed(java.awt.event.ActionEvent evt) {
+        String url = urlTf.getText();
+        if (url != null && !url.equals("")) {
+            createHTMLFile(url);
+        }
+    }
+
+    private void eraseBtnActionPerformed(java.awt.event.ActionEvent evt) {
+        urlTf.setText("");
+        executeResultLbl.setText("추출준비");
+    }
+
+    private void urlTfActionPerformed(java.awt.event.ActionEvent evt) {
+
+    }
+
+    private void urlTfKeyReleased(java.awt.event.KeyEvent evt) {
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            // SwingUtilities.getWindowAncestor(evt.getComponent()).dispose();
+            String url = urlTf.getText();
+            if (url != null && !url.equals("")) {
+                createHTMLFile(url);
+            }
+        }
+    }
+
+    public static StringBuilder createHTMLFile(String url) {
+
+        News gurl = new News();
+        gurl.getURL(url);
+        String protocol = gurl.getProtocol();
+        String host = gurl.getHost();
+        String path = gurl.getPath();
+        String protocolHost = gurl.getProtocolHost();
+
+        StringBuilder sb1 = new StringBuilder();
+        Document doc;
+        String strTitleForFileName;
+        try {
+            doc = Jsoup.connect(url).get();
+            doc.select("iframe").remove();
+            doc.select("script").remove();
+            doc.select("ins").remove();
+            doc.select(".btn_info").remove();
+
+            JsoupChangeAhrefElementsAttribute.changeAhrefElementsAttribute(doc, protocol, host, path);
+            JsoupChangeImageElementsAttribute.changeImageElementsAttribute(doc, protocol, host, path);
+            JsoupChangeLinkHrefElementsAttribute.changeLinkHrefElementsAttribute(doc, protocol, host, path);
+            JsoupChangeScriptSrcElementsAttribute.changeScriptSrcElementsAttribute(doc, protocol, host, path);
+
+            Elements title = doc.select(".con_article .headline h2");
+            strTitle = title.get(0).text();
+            logger.debug("title:" + strTitle);
+            strTitleForFileName = strTitle;
+            strTitleForFileName = StockUtil.getTitleForFileName(strTitleForFileName);
+            logger.debug("strTitleForFileName:" + strTitleForFileName);
+
+            Element authorEl = doc.select(".info_reporter").get(0);
+            String strAuthorHtml= authorEl.html();
+            logger.debug("authorHtml:[" + strAuthorHtml + "]");
+            String strAuthor= authorEl.text();
+            logger.debug("author:[" + strAuthor + "]");
+
+            Element dateEl = doc.select(".article_info").get(0);
+            String dateHtml = dateEl.html();
+            logger.debug("dateHtml:[" + dateHtml + "]");
+            
+            String strDate = doc.select(".article_info .url_txt:first-child").get(0).text();
+            strDate = strDate.replace("입력", "").trim();
+            logger.debug("strDate:[" + strDate + "]");
+            String strFileNameDate = strDate;
+            strFileNameDate = StockUtil.getDateForFileName(strDate);
+            logger.debug("strFileNameDate:" + strFileNameDate);
+
+            Element articleSummary = doc.select(".article_summary").get(0);
+            String strArticleSummaryHtml = articleSummary.html();
+            logger.debug("strArticleSummaryHtml:"+strArticleSummaryHtml);
+            Element articleEl = doc.select(".con_article .article").get(0);
+            String articleHtml = articleEl.outerHtml();
+            logger.debug("contentHtml:"+articleHtml);
+            String strArticle = StockUtil.makeStockLinkStringByExcel(articleHtml);
+            logger.debug("strArticle:"+strArticle);
+
+            String strCopyright = doc.select(".article_copy").outerHtml();
+
+            sb1.append("<html lang='ko'>\r\n");
+            sb1.append("<head>\r\n");
+            //sb1.append("<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">\r\n");
+            sb1.append("</head>\r\n");
+            sb1.append("<body>\r\n");
+
+            sb1.append(StockUtil.getMyCommentBox());
+
+            sb1.append("<div style='width:548px'>\r\n");
+
+            sb1.append("<h3> 기사주소:[<a href='" + url + "' target='_sub'>" + url + "</a>] </h3>\n");
+            sb1.append("<h4>[" + strDate + "] " + strTitle + "</h4>\n");
+            sb1.append("<h5>" + strAuthorHtml + "</h5>\n");
+            sb1.append("<h4>" + strArticleSummaryHtml + "</h4>\n");
+            sb1.append(strArticle + "<br>\r\n");
+            sb1.append(strCopyright + "<br>\r\n");
+
+            sb1.append("</div>\r\n");
+            sb1.append("</body>\r\n");
+            sb1.append("</html>\r\n");
+
+            File dir = new File(userHome + File.separator + "documents" + File.separator + host);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            String fileName = userHome + File.separator + "documents" + File.separator + strFileNameDate + "_" + strTitleForFileName + ".html";
+            FileUtil.fileWrite(fileName, sb1.toString());
+
+            fileName = userHome + File.separator + "documents" + File.separator + strFileNameDate + "_" + strTitleForFileName + ".html";
+            FileUtil.fileWrite(fileName, sb1.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            logger.debug("추출완료");
+            if (executeResultLbl != null) {
+                executeResultLbl.setText("추출완료");
+            }
+        }
+        return sb1;
+    }
+
+}
