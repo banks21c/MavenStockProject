d6df9848a4adbff5a7aa5eb876923c627dc26eed
banksfamily <banksfamily@192.168.1.86>
2020-06-24 19:26:33 +0900

    수정

--- src/main/java/html/parsing/stock/CalcRatioTest.java
+++ src/main/java/html/parsing/stock/CalcRatioTest.java
@@ -3,23 +3,22 @@ package html.parsing.stock;
 public class CalcRatioTest {
 
 	public CalcRatioTest() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		String sTotalSpecificDayRetainAmount  = "3,022,074,824,400";
-		double lTotalSpecificDayRetainAmount = Double.parseDouble(sTotalSpecificDayRetainAmount.replaceAll(",", ""));
+		String sTotalChosenDayRetainAmount  = "3,022,074,824,400";
+		double lTotalChosenDayRetainAmount = Double.parseDouble(sTotalChosenDayRetainAmount.replaceAll(",", ""));
 		String sRetainAmount  = "3,947,082,517,800";
 		double lRetainAmount = Double.parseDouble(sRetainAmount.replaceAll(",", ""));
-		double t = (lTotalSpecificDayRetainAmount - lRetainAmount);
+		double t = (lTotalChosenDayRetainAmount - lRetainAmount);
 
-		double temp1 = t / lTotalSpecificDayRetainAmount;
+		double temp1 = t / lTotalChosenDayRetainAmount;
 		double temp2 = temp1 * 100.0;
 		double gapRatio = Math.round(temp2 * 100)/100.0;
 		System.out.println("gapRatio :"+gapRatio+"%");
 
-		double gapRatio2 = Math.round((lTotalSpecificDayRetainAmount - lRetainAmount)/ lTotalSpecificDayRetainAmount * 100* 100)/100.0;
-		if(lTotalSpecificDayRetainAmount < lRetainAmount) {
+		double gapRatio2 = Math.round((lTotalChosenDayRetainAmount - lRetainAmount)/ lTotalChosenDayRetainAmount * 100* 100)/100.0;
+		if(lTotalChosenDayRetainAmount < lRetainAmount) {
 			gapRatio2 = -gapRatio2;
 		}else {
 			gapRatio2 = gapRatio2;
